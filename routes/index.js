05c476391b29bfd7b3f5bdc93e9ccd087a000f9d
Nisarg Shah <shah.nisarg39@gmail.com>
2023-06-06 12:39:14 +0530

    version 1

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const homeController = require('../controller/homecontroller');
+router.get('/',homeController.home);
+
+router.post('/add', homeController.add);
+
+router.get('/status/:habit_id&:date_id&:mark', homeController.status);
+
+router.get('/delete/:id', homeController.deleteTask);
+
+// console.log('router is loaded')
+module.exports = router;
